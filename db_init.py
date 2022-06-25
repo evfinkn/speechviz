6264be03eedfbd07222acecaae57dce5ee096c95
efinkn <evan-finken@uiowa.edu>
2022-06-24 20:31:23 -0500

    Fix database name

--- db_init.py
+++ db_init.py
@@ -3,19 +3,12 @@ import os
 
 def init_database():
     try:
-        os.remove('clinic.sqlite3')
+        os.remove('speechviz.sqlite3')
     except FileNotFoundError:
         pass
 
     conn = sqlite3.connect('speechviz.sqlite3')
     c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS wavefiles(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    audiofile TEXT,
-                    datafile TEXT,
-                    UNIQUE(audiofile))''')
-    conn.commit()
 
     # create the users and include the passwords
     c.execute('''CREATE TABLE IF NOT EXISTS users(
@@ -29,16 +22,15 @@ def init_database():
 
     # create annotations tables
     c.execute('''CREATE TABLE IF NOT EXISTS annotations(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            file_id INTEGER,
-            user_id INTEGER,
-            start REAL,
-            end REAL,
-            label TEXT,
-            UNIQUE(user_id, file_id, id),
-            FOREIGN KEY(file_id) references wavefiles(id),
-            FOREIGN KEY(user_id) references users(id)
-            )''')
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        audiofile TEXT,
+        user_id INTEGER,
+        start REAL,
+        end REAL,
+        label TEXT,
+        UNIQUE(id),
+        FOREIGN KEY(user_id) references users(id)
+    )''')
 
     conn.close()
 
