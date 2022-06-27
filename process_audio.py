b07c144c61248d09a10ea23c173af63fefe32de6
Blake Hohmann <blake-hohmann@uiowa.edu>
2022-06-27 10:38:03 -0500

    Update process_audio.py

--- process_audio.py
+++ process_audio.py
@@ -187,7 +187,7 @@ def process_audio(*args,
                 )
             # sort speakers so that index of 'Speaker i' is i - 1
             #ADD SNR IN BETWEEN SPEAKER, SPEAKERS_SEGMENTS,
-            speakers_segments = [(speaker, speakers_segments[speaker]) for speaker in sorted(speakers_segments), findSpeakerSNR(speaker)]
+            speakers_segments = [(speaker, speakers_segments[speaker], findSpeakerSNR(speaker)) for speaker in sorted(speakers_segments)]
             segments.append(("Speakers", speakers_segments))
             
         if do_vad:
