9dafdad74f9fbc9686ab767c028cbced51953b7c
Blake Hohmann <blake-hohmann@uiowa.edu>
2022-07-07 09:48:36 -0500

    Update process_audio.py

--- process_audio.py
+++ process_audio.py
@@ -66,13 +66,14 @@ def get_noise_times(signal_times, noise_times):
         left=noise_times[0]
         right=noise_times[1]
         for noiseSegTuple in noise_times:
-            print(noiseSegTuple)
             if noiseSegTuple[0] - start < left[1] - start:
                 left = noiseSegTuple
-                print(left)
+                print("the signal time is: " + str(noiseSegTuple))
+                print("the new left noise is: " + str(left))
             if noiseSegTuple[1] - stop < right[0] - stop:
                 right = noiseSegTuple
-                print(right)
+                print("the signal time is: " + str(noiseSegTuple))
+                print("the new right noise is:" + str(right))
         #left = (np.abs(noise_times - start)).argmin()
         #right = (np.abs(noise_times - stop)).argmin()
         #if right == left:
