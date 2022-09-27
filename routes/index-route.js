de11b6606a3de22ea8c27927577fe981d4ea18a9
efinkn <efinkn@uiowa.edu>
2022-09-27 16:40:58 -0500

    Remove unused variable

--- routes/index-route.js
+++ routes/index-route.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var db = require('better-sqlite3')("speechviz.sqlite3");
 
 /* GET home page. */
 router.get('/', (req, res, next) => res.render("index"));
