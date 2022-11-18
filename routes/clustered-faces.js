7657165cc57be2458a1ee79fe6e08b81af3af059
blhohmann <blake-hohmann@uiowa.edu>
2022-11-18 00:29:03 -0600

    messed that up

--- routes/clustered-faces.js
+++
@@ -1,9 +0,0 @@
-doctype html
-html(lang="en")
-    head
-        title Speechviz
-    body
-        fieldset#user-selection(hidden=true)
-            legend(style="font-size:20px;") Select a user:
-        fieldset#file-selection 
-            legend(style="font-size:20px;") Clustered Faces:
