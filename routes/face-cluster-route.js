837eb20a0df4c41e22549915eb655e5350e0362f
blhohmann <blake-hohmann@uiowa.edu>
2022-11-23 01:05:45 -0600

    Update routes/face-cluster-route.js

--- routes/face-cluster-route.js
+++ routes/face-cluster-route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const fs = require("fs");
 
 
-var dir = "data/faceClusters/video1.mp4/testLabel0/"
+var dir = "faceClusters/video1.mp4/testLabel0/"
 
 
 /* GET home page. */
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
     file = req.query.file;
     test = req.query.test;
     
-    fs.readdir(dir, function(err, files){
+    fs.readdir(("data/"+ dir), function(err, files){
         console.log(files);
         res.render("facecluster", { "images": files, "dir": dir });//send to views/facecluster.pug
     });
