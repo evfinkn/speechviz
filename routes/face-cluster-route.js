914d60ed1ee03c6a159dab610bc52cb878e433b0
blhohmann <blake-hohmann@uiowa.edu>
2022-11-22 23:30:25 -0600

    working on face cluster route

--- routes/face-cluster-route.js
+++ routes/face-cluster-route.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => res.render("facecluster"));
+router.get('/', (req, res, next) => {
+    res.render("facecluster");
+    file = req.query.file;
+    
+});
 
 module.exports = router;
