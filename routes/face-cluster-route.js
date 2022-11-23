23f6ec42292b1e15d389aedb160be7402ca3418a
blhohmann <blake-hohmann@uiowa.edu>
2022-11-23 00:37:28 -0600

    testing if i can send a directory to face cluster pug

--- routes/face-cluster-route.js
+++ routes/face-cluster-route.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var dir = /faceClusters/video1.mp4/testLabel0/
+
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render("facecluster");
     file = req.query.file;
+    test = req.query.test;
     
+    fs.readdir(dir, function(err, files){
+        res.render
+        res.render("clusteredFaces", { images: files, directory: dir });
+    });
 });
 
 module.exports = router;
