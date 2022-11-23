0e51bd4966358e08ffd93e336426e345e2cc67a9
blhohmann <blake-hohmann@uiowa.edu>
2022-11-23 02:47:41 -0600

    Update src/faceCluster.js

--- src/faceCluster.js
+++ src/faceCluster.js
@@ -8,20 +8,8 @@ fetch("/clustered-faces")
     })
     .then(fileList => { 
         const clusterFiles = fileList.cluster;
-        
-        if (clusterFiles?.length !== 0) {
-            const fieldset = document.getElementById("file-selection");
-
-            fieldset.append(htmlToElement("<strong>Clustered Faces</strong>"));  // header for video files
-            clusterfolders.forEach(function (folderName){
-                const div = htmlToElement(`<div><input type="radio" id="${folderName}cluster"" name="file-selection" value="${folderName}cluster"></input><label for="${folderName}cluster">${folderName}</label></div>`);
-                div.firstElementChild.addEventListener("change", function () {
-                    // when radio button clicked, open that video file in viz
-                    window.location.replace(`/clustered-faces?${user ? "user=" + user + "&" : ""}file=${this.value.replace('cluster', '')}`);
-                });
-                fieldset.append(div);
-            });
-        }
+        const fieldset = document.getElementById("file-selection");
+        fieldset.append(htmlToElement("<strong>Clustered Faces</strong>"));
     })
     .catch(error => { console.error('Error during fetch: ', error); });  // catch err thrown by res if any
 
