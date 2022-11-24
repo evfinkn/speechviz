8270d537a3f592dc30b8001ade397c2baa8d5a5d
blhohmann <blake-hohmann@uiowa.edu>
2022-11-24 14:55:23 -0600

    Update src/faceCluster.js

--- src/faceCluster.js
+++ src/faceCluster.js
@@ -11,18 +11,18 @@ fetch("/clustered-faces")
         const fieldset = document.getElementById("file-selection");
         fieldset.append(htmlToElement("<strong>Clustered Faces</strong>"));
 
-        if (clusterfolders?.length !== 0){
-            fieldset.append(htmlToElement("<strong>Clustered Faces</strong>"));  // header for video files
-            clusterfolders.forEach(function (folderName){
-                const div = htmlToElement(`<div><input type="radio" id="${folderName}cluster"" name="file-selection" value="${folderName}cluster"></input><label for="${folderName}cluster">${folderName}</label></div>`);
-                div.firstElementChild.addEventListener("change", function () {
+        //if (clusterfolders?.length !== 0){
+            //fieldset.append(htmlToElement("<strong>Clustered Faces</strong>"));  // header for video files
+            //clusterfolders.forEach(function (folderName){
+                //const div = htmlToElement(`<div><input type="radio" id="${folderName}cluster"" name="file-selection" value="${folderName}cluster"></input><label for="${folderName}cluster">${folderName}</label></div>`);
+                //div.firstElementChild.addEventListener("change", function () {
                     // when radio button clicked, open that video file in viz
-                    console.log(this.value);
-                    window.location.replace(`/clustered-faces?${user ? "user=" + user + "&" : ""}dir=${this.value.replace('cluster', '')}&inFaceFolder=true`);
-                });
-                fieldset.append(div);
-            });
-        }
+                    //console.log(this.value);
+                    //window.location.replace(`/clustered-faces?${user ? "user=" + user + "&" : ""}dir=${this.value.replace('cluster', '')}&inFaceFolder=true`);
+                //});
+                //fieldset.append(div);
+            //});
+        //}
 
         //when button clicked to go to folder 
         window.location.replace(`/clustered-faces?faceFolder=${this.value.replace('cluster', '')}&inFaceFolder=true`);
