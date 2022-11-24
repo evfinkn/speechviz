6539f442b7a6fef7d10a04dc782eb2a0da66c995
blhohmann <blake-hohmann@uiowa.edu>
2022-11-24 14:29:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get("/clustered-faces", (req, res) => {
   const exclude = new Set([".DS_Store"]);  // I just used a Set because Set.has() is faster than Array.includes()
   faceFolder = req.query.faceFolder
   const files = {};
-  files.cluster = fs.readdirSync("data/faceClusters").filter(fileName => !exclude.has(fileName));
+  //files.cluster = fs.readdirSync("data/faceClusters").filter(fileName => !exclude.has(fileName));
   files.faceFolder = faceFolder;
   res.send(files);
 });
