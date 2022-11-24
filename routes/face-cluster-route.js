afd5dc06b6ad475feef3d797950ee90ee91ba5c6
blhohmann <blake-hohmann@uiowa.edu>
2022-11-24 02:15:52 -0600

    Update routes/face-cluster-route.js

--- routes/face-cluster-route.js
+++ routes/face-cluster-route.js
@@ -7,21 +7,33 @@ const fs = require("fs");
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    folder = req.query.folder;
-    faceFolder = req.query.faces;
+    folder = req.query.dir;
+    faceFolder = req.query.faceFolder;
+    overallFolder = req.query.inFaceFolder
     console.log(folder);
     console.log(faceFolder);
+    console.log(overallFolder);
+
     var dir = "faceClusters/" + folder + "/"
     var faces;
 
+    //find folders of faces, and pass that 
     fs.readdir(("data/" + dir), function(err, files){
         faces = files;
     });
+
+
+    if (overallFolder){ //user needs to pick which face folder to view
+        res.render("facecluster", { "dir": dir, "faces": faces, inFaceFolder: false });//send to views/facecluster.pug
+    }
+    else { //we are in a face folder so face folder is defined, and can therefore send names of the images to pug to render
+        fs.readdir(("data/"+ dir + faceFolder + "/"), function(err, files){
+            console.log(faces);
+            res.render("facecluster", { "images": files, "dir": dir, "faceFolder": faceFolder, "faces": faces, inFaceFolder: true });//send to views/facecluster.pug
+        });
+    }
     
-    fs.readdir(("data/"+ dir + "testLabel0/"), function(err, files){
-        console.log(faces);
-        res.render("facecluster", { "images": files, "originalDirectory": dir, "dir": dir, "faces": faces, inFaceFolder: false });//send to views/facecluster.pug
-    });
+    
 });
 
 module.exports = router;
