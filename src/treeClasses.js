5aa95ed903ad88ea391b4168e22dfc6f4320c8d2
blhohmann <blake-hohmann@uiowa.edu>
2022-11-27 16:51:45 -0600

    Update src/treeClasses.js

--- src/treeClasses.js
+++ src/treeClasses.js
@@ -643,7 +643,7 @@ var Popup = class Popup {
         radioButton.addEventListener("change", () => {
             //add functionality to associate with speakers here
             console.log("associate, function not provided yet");
-            console.log(this.treeItem.li);
+            console.log(this.treeItem.li.children);
 
             radioButton.checked = false;
             this.hide();
