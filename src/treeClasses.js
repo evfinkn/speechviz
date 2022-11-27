9da0bb58dcf810e91f626192b21960bc380a67e4
blhohmann <blake-hohmann@uiowa.edu>
2022-11-27 16:06:52 -0600

    Update src/treeClasses.js

--- src/treeClasses.js
+++ src/treeClasses.js
@@ -1411,7 +1411,7 @@ var Face  = class Face extends TreeItem {
         });
 
         const imageLi = htmlToElement(`<li><img src='faceClusters/${imagePath}' alt='Example image of face'/></li>`);
-        nest = this.li.lastElementChild;
+        var nest = this.li.lastElementChild;
         nest.appendChild(imageLi);
         this.popup = new Popup(this);
     }
