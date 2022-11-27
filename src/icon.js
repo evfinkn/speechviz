594464e0c84a74203d532528877b9cee9734f9b8
blhohmann <blake-hohmann@uiowa.edu>
2022-11-27 12:56:21 -0600

    Update src/icon.js

--- src/icon.js
+++ src/icon.js
@@ -4,9 +4,8 @@ const segmentPlayIcon = feather.icons.play.toSvg({ "width": 12, "height": 12, "s
 const segmentPauseIcon = feather.icons.pause.toSvg({ "width": 12, "height": 12, "stroke": "black", "fill": "black" });
 const segmentLoopIcon = feather.icons.repeat.toSvg({ "width": 12, "height": 12, "stroke": "black", "stroke-width": 2.5 });
 const segmentRemoveIcon = feather.icons.x.toSvg({ "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 });
-const segmentIcons = { "play": segmentPlayIcon, "pause": segmentPauseIcon, "loop": segmentLoopIcon, "remove": segmentRemoveIcon }
-
-const faceIcons = { "remove": segmentRemoveIcon }
+const faceImgIcon = feather.icons.image.toSvg({ "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 });
+const segmentIcons = { "play": segmentPlayIcon, "pause": segmentPauseIcon, "loop": segmentLoopIcon, "remove": segmentRemoveIcon, "image": faceImgIcon }
 
 const groupPlayIcon = feather.icons.play.toSvg({ "width": 15, "height": 15, "stroke": "black", "fill": "black" });
 const groupPauseIcon = feather.icons.pause.toSvg({ "width": 15, "height": 15, "stroke": "black", "fill": "black" });
@@ -22,4 +21,4 @@ const redoIcon = feather.icons["corner-up-right"].toSvg({ "stroke": "gray" });
 
 const settingsIcon = feather.icons.settings.toSvg({ "stroke": "black" });
 
-export { segmentIcons, groupIcons, faceIcons, zoomInIcon, zoomOutIcon, undoIcon, redoIcon, settingsIcon };
+export { segmentIcons, groupIcons, zoomInIcon, zoomOutIcon, undoIcon, redoIcon, settingsIcon };
