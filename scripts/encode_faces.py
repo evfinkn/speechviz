67b447212481bf3bda97bb8ff70bdb35cf084233
blhohmann <blake-hohmann@uiowa.edu>
2022-12-08 00:11:18 -0600

    Update scripts/encode_faces.py

--- scripts/encode_faces.py
+++ scripts/encode_faces.py
@@ -10,13 +10,14 @@ from PIL import Image
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", required=True,
+ap.add_argument("-i", "--dataset", type=str, required=True,
 	help="path to input directory of faces + images")
-ap.add_argument("-e", "--encodings", required=True,
+ap.add_argument("-e", "--encodings", type=str, required=True,
 	help="path to serialized db of facial encodings")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
 	help="face detection model to use: either `hog` or `cnn`")
-ap.add_argument("-o", "--outputs", required=True)
+ap.add_argument("-o", "--outputs", type=str, required=False,
+	help="If given, will output detected faces to this folder")
 args = vars(ap.parse_args())
 
 # grab the paths to the input images in our dataset, then initialize
@@ -25,24 +26,24 @@ print("[INFO] quantifying faces...")
 imagePaths = list(paths.list_images(args["dataset"]))
 data = []
 
-if not os.path.isdir(args["outputs"]):
-    os.makedirs(args["outputs"])
+if args["outputs"] is not None:
+	if not os.path.isdir(args["outputs"]):
+		os.makedirs(args["outputs"])
 
 counter = 0	
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
 	# load the input image and convert it from RGB (OpenCV ordering)
 	# to dlib ordering (RGB)
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
-	print(imagePath)
+	if (i + 1) % 100 == 0:
+		print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+		print(imagePath)
 	image = cv2.imread(imagePath)
 	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 	# detect the (x, y)-coordinates of the bounding boxes
 	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model=args["detection_method"])
+	boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
 
 	# compute the facial embedding for the face
 	encodings = face_recognition.face_encodings(rgb, boxes)
@@ -53,17 +54,16 @@ for (i, imagePath) in enumerate(imagePaths):
 		for (box, enc) in zip(boxes, encodings)]
 	data.extend(d)
 
-	print("I found {} face(s) in this photograph.".format(len(boxes)))
-	#uncomment below to see what faces are detected
-	#for box in boxes:
-    		# Print the location of each face in this image
-    		#top, right, bottom, left = box
+	if args["outputs"] is not None:
+		print("There were {} face(s) in this image.".format(len(boxes)))
+		for box in boxes:
+			#Print the location of each face in this image
+			top, right, bottom, left = box
 
-    		# You can access the actual face itself like this:
-    		#face_image = image[top:bottom, left:right]
-    		#pil_image = np.array(Image.fromarray(face_image))
-    		#cv2.imwrite(args["outputs"] + "/Num" + str(counter) + ".jpg", pil_image)
-    		#counter += 1
+			face_image = image[top:bottom, left:right]
+			pil_image = np.array(Image.fromarray(face_image))
+			cv2.imwrite(args["outputs"] + "/Num" + str(counter) + ".jpg", pil_image)
+			counter += 1
 
 # dump the facial encodings data to disk
 print("[INFO] serializing encodings...")
