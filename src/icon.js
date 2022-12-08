109c6eea342fced0dc132d176d2612e189afd902
blhohmann <blake-hohmann@uiowa.edu>
2022-12-08 10:04:06 -0600

    fixing line length

--- src/icon.js
+++ src/icon.js
@@ -1,17 +1,25 @@
 const feather = require('feather-icons');
 
-const segmentPlayIcon = feather.icons.play.toSvg({ "width": 12, "height": 12, "stroke": "black", "fill": "black" });
-const segmentPauseIcon = feather.icons.pause.toSvg({ "width": 12, "height": 12, "stroke": "black", "fill": "black" });
-const segmentLoopIcon = feather.icons.repeat.toSvg({ "width": 12, "height": 12, "stroke": "black", "stroke-width": 2.5 });
-const segmentRemoveIcon = feather.icons.x.toSvg({ "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 });
-const faceImgIcon = feather.icons.image.toSvg({ "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 });
-const segmentIcons = { "play": segmentPlayIcon, "pause": segmentPauseIcon, "loop": segmentLoopIcon, "remove": segmentRemoveIcon, "image": faceImgIcon }
+const segmentOptions = { "width": 12, "height": 12, "stroke": "black", "fill": "black" }
+const segmentOptionsWidth = { "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 }
+const segmentPlayIcon = feather.icons.play.toSvg(segmentOptions);
+const segmentPauseIcon = feather.icons.pause.toSvg(segmentOptions);
+const segmentLoopIcon = feather.icons.repeat.toSvg(segmentOptionsWidth);
+const segmentRemoveIcon = feather.icons.x.toSvg(segmentOptionsWidth);
+const faceImgIcon = feather.icons.image.toSvg(segmentOptionsWidth);
+const segmentIcons = { "play": segmentPlayIcon, "pause": segmentPauseIcon, 
+                       "loop": segmentLoopIcon, "remove": segmentRemoveIcon, 
+                       "image": faceImgIcon }
 
-const groupPlayIcon = feather.icons.play.toSvg({ "width": 15, "height": 15, "stroke": "black", "fill": "black" });
-const groupPauseIcon = feather.icons.pause.toSvg({ "width": 15, "height": 15, "stroke": "black", "fill": "black" });
-const groupLoopIcon = feather.icons.repeat.toSvg({ "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 });
-const groupRemoveIcon = feather.icons.x.toSvg({ "width": 17, "height": 17, "stroke": "black", "stroke-width": 2.5 });
-const groupIcons = { "play": groupPlayIcon, "pause": groupPauseIcon, "loop": groupLoopIcon, "remove": groupRemoveIcon };
+const groupOptions = { "width": 15, "height": 15, "stroke": "black", "fill": "black" }
+const groupOptionsWidth = { "width": 15, "height": 15, "stroke": "black", "stroke-width": 2.5 }
+const groupPlayIcon = feather.icons.play.toSvg(groupOptions);
+const groupPauseIcon = feather.icons.pause.toSvg(groupOptions);
+const groupLoopIcon = feather.icons.repeat.toSvg(groupOptionsWidth);
+const groupRemoveIcon = feather.icons.x.toSvg({ "width": 17, "height": 17, "stroke": "black", 
+                                                "stroke-width": 2.5 });
+const groupIcons = { "play": groupPlayIcon, "pause": groupPauseIcon, "loop": groupLoopIcon, 
+                     "remove": groupRemoveIcon };
 
 const zoomInIcon = feather.icons["zoom-in"].toSvg({ "stroke": "gray" });
 const zoomOutIcon = feather.icons["zoom-out"].toSvg({ "stroke": "black" });
