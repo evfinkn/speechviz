09f240385becefe71ff2d4485d6c3e883a64fbea
blhohmann <blake-hohmann@uiowa.edu>
2022-12-08 01:05:25 -0600

    cleaning up and fixing line length

--- routes/face-cluster-route.js
+++ routes/face-cluster-route.js
@@ -2,50 +2,41 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs");
 
-
-
-
 /* GET home page. */
 router.get('/', (req, res, next) => {
     
-    var folder;
-    var faceFolder;
-    var inFace;
-
-    if (req.query.inFaceFolder == "false"){ //always in url to update
-        inFace = false;
-    }
-    else{
-        inFace = true;
-    }
-    req.session.inFaceFolder = inFace;
+    var inFace = req.query.inFaceFolder == "false" ? false : true;
+    req.session.inFaceFolder = inFace; //for app.js
     
-    if (typeof req.query.faceFolder !== 'undefined'){//if faceFolder is in url they just selected a cluster to view
+    var faceFolder;
+    if (typeof req.query.faceFolder !== 'undefined'){//face folder changed
         faceFolder = req.query.faceFolder;
-        req.session.faceFolder = faceFolder;
+        req.session.faceFolder = faceFolder; //for app.js get images for face folder we are in
     }
-    else if (typeof req.session.faceFolder !== 'undefined'){//if faceFolder is already defined and wasn't in url keep it the same
+    else if (typeof req.session.faceFolder !== 'undefined'){//keep face folder the same
         faceFolder = req.session.faceFolder;
     }
 
-    if (typeof req.query.dir !== 'undefined'){ //update dir if it is in url
+    var folder;
+    if (typeof req.query.dir !== 'undefined'){ //video changed
         folder = req.query.dir;
         req.session.dir = folder;
     }
     else { //keep the same dir
         folder = req.session.dir;
     }
-
     var dir = "faceClusters/" + folder + "/"
 
     if (!inFace){ //user needs to pick which face folder to view
         req.session.inFaceFolder = false;
-        res.render("facecluster", { "dir": dir, inFaceFolder: false });//send to views/facecluster.pug
+        res.render("facecluster", { "dir": dir, inFaceFolder: false });//send to faceCluster.pug
     }
-    else { //we are in a face folder so face folder is defined, and can therefore send names of the images to pug to render
+    else { //we are in a face folder, and can therefore send paths of the images to pug to render
         fs.readdir(("data/"+ dir + faceFolder + "/"), function(err, files){
             req.session.inFaceFolder = true;
-            res.render("facecluster", { "images": files, "faceFolder": faceFolder, "dir": dir, "faceFolder": faceFolder,  inFaceFolder: true });//send to views/facecluster.pug
+            res.render("facecluster", { "images": files, "faceFolder": faceFolder, "dir": dir,
+                                        "faceFolder": faceFolder,  inFaceFolder: true });
+            //send to views/facecluster.pug
         });
     }
 });
