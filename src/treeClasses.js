d8bd6096c7c549e159afecbfe8d7af6a014f09de
blhohmann <blake-hohmann@uiowa.edu>
2022-12-08 16:52:16 -0600

    Update src/treeClasses.js

--- src/treeClasses.js
+++ src/treeClasses.js
@@ -1798,13 +1798,13 @@ var Face  = class Face extends TreeItem {
         this.parent = parent;
         this.playButton.style.display = "none";
         this.loopButton.style.display = "none";
-        console.log(segmentIcons.icons);
+        console.log(icons);
         //rel="noopener noreferrer" is there to avoid tab nabbing
         const linkButton = htmlToElement(`<a href="/clustered-faces?faceFolder=`
                                          + `${this.id}&inFaceFolder="true"` 
                                          + ` style="text-decoration:none;"`
                                          + ` target="_blank" rel="noopener noreferrer">`
-                                         + ` ${segmentIcons.icons.play}</a>`);
+                                         + ` ${icons.play}</a>`);
 
         this.linkbutton = linkButton;
         this.removeButton.after(linkButton);
