610092416b32f7e40dfe709c5b96c14b2b9af63f
efinkn <efinkn@uiowa.edu>
2023-01-12 10:55:11 -0600

    Fix error and add printing utility

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -8,28 +8,40 @@ if [[ "$PYANNOTE_AUTH_TOKEN" == "" ]]; then
 	exit 1
 fi
 
+print_section_header () {
+	printf "\n"
+	# print "=" 40 times - see https://stackoverflow.com/a/5349842
+	printf "=%.0s" {1..40}
+	# print the first argument
+	printf "\n\n$1\n\n"
+	printf "=%.0s" {1..40}
+	# printf doesn't add a \n so need to output 2 \n to add a blank line
+	# and then to put the next output on the line after the blank line
+	printf "\n\n"
+}
+
 # Currently there's an issue with numpy 1.24 and numba that breaks a lot of things,
 # including pyannote.audio.Pipeline. aria_data_tools installs the latest numpy
 # (1.24.1 at the time of this comment) so we need to downgrade it until the issue
 # is fixed. See https://github.com/numba/numba/issues/8615
+print_section_header "INSTALLING NUMPY"
 pip3 install --force-reinstall "numpy<1.24"
 
-# -E on sudo to preserve environment variables like http_proxy
+print_section_header "INSTALLING APT PACKAGES"
 sudo apt install --no-install-recommends -y \
 	wget ffmpeg \
 	libboost-program-options-dev libboost-regex-dev \
 	libmad0-dev libid3tag0-dev libsndfile1-dev libgd-dev
 
+# aria_data_tools installs node but we want a newer version
+print_section_header "INSTALLING NODE"
 wget -qO- https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt install --no-install-recommends -y nodejs
 
 sudo apt clean
 
-# sudo -E pip3 install --no-cache-dir -U pip
-# sudo -E pip3 install --no-cache-dir \
-# 	typing dataclasses pytest parameterized
-
 # install audiowaveform
+print_section_header "INSTALLING AUDIOWAVEFORM"
 cd /tmp
 git clone https://github.com/bbc/audiowaveform.git
 cd audiowaveform
@@ -41,6 +53,9 @@ cd build
 cmake -D ENABLE_TESTS=0 ..
 sudo make -j$(nproc) install
 rm -rf /tmp/audiowaveform
+
+# print this here so that if the pip3 install in the if runs, it's under this header
+print_section_header "INSTALLING DLIB"
 
 DDLIB_USE_CUDA=0
 EXTRA_INDEX_URL="https://download.pytorch.org/whl/cpu"
@@ -64,12 +79,12 @@ python3 setup.py install
 rm -rf /tmp/dlib
 
 # install speechviz
+print_section_header "INSTALLING SPEECHVIZ"
 cd /speechviz
 sudo pip3 install --no-cache-dir \
-	--extra-index-url $EXTRA_INDEX_URL
+	--extra-index-url $EXTRA_INDEX_URL \
 	-r requirements.txt
-# sudo and --unsafe-perm so that post-install (patch-package) works
-sudo npm install --unsafe-perm
+npm install
 npm run mkdir
 # remove data directory created by npm run mkdir because user will mount in their data
 # speechviz.sqlite3 should be mounted as well, so don't run python3 scripts/db_init.py
