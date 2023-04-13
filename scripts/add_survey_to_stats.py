f47c2d799798dafa0f2fc52b84bf2c35909cb115
evfinkn <evan@finken.name>
2023-04-12 22:16:02 -0500

    Clean up code

--- scripts/add_survey_to_stats.py
+++ scripts/add_survey_to_stats.py
@@ -1,46 +1,35 @@
 import argparse
 import csv
-import os
 import pathlib
 
-from util import add_to_csv
+import util
 
 
-def main(
-    input: pathlib.Path,
-):
-    grabFolder = str(input).split("_")
-    folder = grabFolder[2].split(".")[0]
-    print(folder)
-    oldCsvFolder = os.path.join("data", "stats", folder)
-
-    with open(args["input"]) as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        i = 0
+def main(survey_path: pathlib.Path, stats_path: pathlib.Path):
+    with survey_path.open(newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
         for row in reader:
-            if i != 0:
-                run = row[3]
-                typeScale = row[4]
-                score = row[6]
-                dicti = {"type": typeScale, "eVal": score}
-                add_to_csv(
-                    pathlib.Path(
-                        os.path.join(oldCsvFolder, "run" + run + "-stats.csv")
-                    ),
-                    dicti,
-                )
-            i += 1
+            stats_file = stats_path / f"run{row['run']}-stats.csv"
+            data = {"type": row["type"], "eVal": row["eVal"]}
+            util.add_to_csv(stats_file, data)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Add survey stats to the csv")
+    parser = argparse.ArgumentParser(
+        description="Add survey data to the stats csv files."
+    )
     # path to survey data
     parser.add_argument(
-        "-i",
-        "--input",
+        "survey_path",
         type=pathlib.Path,
-        required=True,
-        help="path to uiowa survey data",
+        help=(
+            "Path to the .csv file containing the survey data. The first row must be"
+            " column headers and the file must have at least the following columns:"
+            " run, type, eVal."
+        ),
+    )
+    parser.add_argument(
+        "stats_path", type=pathlib.Path, help="Path to the data/stats folder."
     )
     args = vars(parser.parse_args())
     main(**args)
