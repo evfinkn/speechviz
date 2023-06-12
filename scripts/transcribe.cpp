f5195f056a56f115f82d2165a8d6656168a60d9e
efinkn <efinkn@uiowa.edu>
2023-06-12 14:37:29 -0500

    Fix transcribe and use new logging functions

--- scripts/transcribe.cpp
+++ scripts/transcribe.cpp
@@ -87,7 +87,8 @@ struct whisper_params {
     std::string prompt;
     std::string model    = "models/ggml-base.en.bin";
 
-    std::vector<std::string> fname_inp = {};
+    std::string fname_inp = "";
+    std::string fname_out = "";
 };
 
 void whisper_print_usage(int argc, char ** argv, const whisper_params & params);
@@ -95,11 +96,6 @@ void whisper_print_usage(int argc, char ** argv, const whisper_params & params);
 bool whisper_params_parse(int argc, char ** argv, whisper_params & params) {
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
-
-        if (arg[0] != '-') {
-            params.fname_inp.push_back(arg);
-            continue;
-        }
 
         if (arg == "-h" || arg == "--help") {
             whisper_print_usage(argc, argv, params);
@@ -123,7 +119,8 @@ bool whisper_params_parse(int argc, char ** argv, whisper_params & params) {
         else if (arg == "-l"    || arg == "--language")       { params.language       = argv[++i]; }
         else if (                  arg == "--prompt")         { params.prompt         = argv[++i]; }
         else if (arg == "-m"    || arg == "--model")          { params.model          = argv[++i]; }
-        else if (arg == "-f"    || arg == "--file")           { params.fname_inp.emplace_back(argv[++i]); }
+        else if (arg == "-i"    || arg == "--input")          { params.fname_inp      = argv[++i]; }
+        else if (arg == "-o"    || arg == "--output")         { params.fname_out      = argv[++i]; }
         else {
             fprintf(stderr, "error: unknown argument: %s\n", arg.c_str());
             whisper_print_usage(argc, argv, params);
@@ -158,7 +155,8 @@ void whisper_print_usage(int /*argc*/, char ** argv, const whisper_params & para
     fprintf(stderr, "  -l LANG,  --language LANG  [%-7s] spoken language ('auto' for auto-detect)\n",       params.language.c_str());
     fprintf(stderr, "            --prompt PROMPT  [%-7s] initial prompt\n",                                 params.prompt.c_str());
     fprintf(stderr, "  -m FNAME, --model FNAME    [%-7s] model path\n",                                     params.model.c_str());
-    fprintf(stderr, "  -f FNAME, --file FNAME     [%-7s] input WAV file path\n",                            "");
+    fprintf(stderr, "  -i FNAME, --input FNAME    [%-7s] input WAV file path\n",                            "");
+    fprintf(stderr, "  -o FNAME, --output FNAME   [%-7s] output JSON file path\n",                          "");
     fprintf(stderr, "\n");
 }
 
@@ -235,7 +233,13 @@ int main(int argc, char ** argv) {
     }
 
     if (params.fname_inp.empty()) {
-        fprintf(stderr, "error: no input files specified\n");
+        fprintf(stderr, "error: no input file specified\n");
+        whisper_print_usage(argc, argv, params);
+        return 2;
+    }
+
+    if (params.fname_out.empty()) {
+        fprintf(stderr, "error: no output file specified\n");
         whisper_print_usage(argc, argv, params);
         return 2;
     }
@@ -271,150 +275,138 @@ int main(int argc, char ** argv) {
         fprintf(stderr, "]\n");
     }
 
-    for (int f = 0; f < (int) params.fname_inp.size(); ++f) {
-        const auto fname_inp = params.fname_inp[f];
-        const std::filesystem::path fpath(fname_inp);
-        const auto grandparent = fpath.parent_path().parent_path();
-        if (grandparent.stem() != "data") {
-            throw std::invalid_argument("Files must be in a subdirectory of the data directory, but " + fpath.string() + " isn't");
-        }
+    const auto fname_inp = params.fname_inp;
+    const auto fname_out = params.fname_out;
 
-        std::vector<float> pcmf32; // mono-channel F32 PCM
-        std::vector<std::vector<float>> pcmf32s; // stereo-channel F32 PCM
+    std::vector<float> pcmf32; // mono-channel F32 PCM
+    std::vector<std::vector<float>> pcmf32s; // stereo-channel F32 PCM
 
-        // WAV input
-        {
-            drwav wav;
-            std::vector<uint8_t> wav_data; // used for pipe input from stdin
+    // WAV input
+    {
+        drwav wav;
+        std::vector<uint8_t> wav_data; // used for pipe input from stdin
 
-            if (fname_inp == "-") {
-                {
-                    uint8_t buf[1024];
-                    while (true)
-                    {
-                        const size_t n = fread(buf, 1, sizeof(buf), stdin);
-                        if (n == 0) {
-                            break;
-                        }
-                        wav_data.insert(wav_data.end(), buf, buf + n);
-                    }
-                }
-
-                if (drwav_init_memory(&wav, wav_data.data(), wav_data.size(), nullptr) == false) {
-                    fprintf(stderr, "error: failed to open WAV file from stdin\n");
-                    return 4;
-                }
-
-                fprintf(stderr, "%s: read %zu bytes from stdin\n", __func__, wav_data.size());
-            }
-            else if (drwav_init_file(&wav, fname_inp.c_str(), nullptr) == false) {
-                fprintf(stderr, "error: failed to open '%s' as WAV file\n", fname_inp.c_str());
-                return 5;
-            }
-
-            if (wav.channels != 1 && wav.channels != 2) {
-                fprintf(stderr, "%s: WAV file '%s' must be mono or stereo\n", argv[0], fname_inp.c_str());
-                return 6;
-            }
-
-            if (params.diarize && wav.channels != 2 && params.no_timestamps == false) {
-                fprintf(stderr, "%s: WAV file '%s' must be stereo for diarization and timestamps have to be enabled\n", argv[0], fname_inp.c_str());
-                return 6;
-            }
-
-            if (wav.sampleRate != WHISPER_SAMPLE_RATE) {
-                fprintf(stderr, "%s: WAV file '%s' must be %i kHz\n", argv[0], fname_inp.c_str(), WHISPER_SAMPLE_RATE/1000);
-                return 8;
-            }
-
-            if (wav.bitsPerSample != 16) {
-                fprintf(stderr, "%s: WAV file '%s' must be 16-bit\n", argv[0], fname_inp.c_str());
-                return 9;
-            }
-
-            const uint64_t n = wav_data.empty() ? wav.totalPCMFrameCount : wav_data.size()/(wav.channels*wav.bitsPerSample/8);
-
-            std::vector<int16_t> pcm16;
-            pcm16.resize(n*wav.channels);
-            drwav_read_pcm_frames_s16(&wav, n, pcm16.data());
-            drwav_uninit(&wav);
-
-            // convert to mono, float
-            pcmf32.resize(n);
-            if (wav.channels == 1) {
-                for (uint64_t i = 0; i < n; i++) {
-                    pcmf32[i] = float(pcm16[i])/32768.0f;
-                }
-            } else {
-                for (uint64_t i = 0; i < n; i++) {
-                    pcmf32[i] = float(pcm16[2*i] + pcm16[2*i + 1])/65536.0f;
-                }
-            }
-
-            if (params.diarize) {
-                // convert to stereo, float
-                pcmf32s.resize(2);
-
-                pcmf32s[0].resize(n);
-                pcmf32s[1].resize(n);
-                for (uint64_t i = 0; i < n; i++) {
-                    pcmf32s[0][i] = float(pcm16[2*i])/32768.0f;
-                    pcmf32s[1][i] = float(pcm16[2*i + 1])/32768.0f;
-                }
-            }
-        }
-
-        // run the inference
-        {
-            whisper_full_params wparams = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
-
-            wparams.print_realtime   = false;
-            wparams.print_progress   = params.print_progress;
-            wparams.print_timestamps = !params.no_timestamps;
-            wparams.print_special    = params.print_special;
-            wparams.translate        = params.translate;
-            wparams.language         = params.language.c_str();
-            wparams.n_threads        = params.n_threads;
-            wparams.n_max_text_ctx   = params.max_context >= 0 ? params.max_context : wparams.n_max_text_ctx;
-            wparams.offset_ms        = params.offset_t_ms;
-            wparams.duration_ms      = params.duration_ms;
-
-            wparams.token_timestamps = params.output_wts || params.max_len > 0;
-            wparams.thold_pt         = params.word_thold;
-            wparams.max_len          = params.output_wts && params.max_len == 0 ? 60 : params.max_len;
-
-            wparams.speed_up         = params.speed_up;
-
-            wparams.prompt_tokens    = prompt_tokens.empty() ? nullptr : prompt_tokens.data();
-            wparams.prompt_n_tokens  = prompt_tokens.empty() ? 0       : prompt_tokens.size();
-
-            // example for abort mechanism
-            // in this example, we do not abort the processing, but we could if the flag is set to true
-            // the callback is called before every encoder run - if it returns false, the processing is aborted
+        if (fname_inp == "-") {
             {
-                static bool is_aborted = false; // NOTE: this should be atomic to avoid data race
-
-                wparams.encoder_begin_callback = [](struct whisper_context * /*ctx*/, void * user_data) {
-                    bool is_aborted = *(bool*)user_data;
-                    return !is_aborted;
-                };
-                wparams.encoder_begin_callback_user_data = &is_aborted;
+                uint8_t buf[1024];
+                while (true)
+                {
+                    const size_t n = fread(buf, 1, sizeof(buf), stdin);
+                    if (n == 0) {
+                        break;
+                    }
+                    wav_data.insert(wav_data.end(), buf, buf + n);
+                }
             }
 
-            if (whisper_full_parallel(ctx, wparams, pcmf32.data(), pcmf32.size(), params.n_processors) != 0) {
-                fprintf(stderr, "%s: failed to process audio\n", argv[0]);
-                return 10;
+            if (drwav_init_memory(&wav, wav_data.data(), wav_data.size(), nullptr) == false) {
+                fprintf(stderr, "error: failed to open WAV file from stdin\n");
+                return 4;
+            }
+
+            fprintf(stderr, "%s: read %zu bytes from stdin\n", __func__, wav_data.size());
+        }
+        else if (drwav_init_file(&wav, fname_inp.c_str(), nullptr) == false) {
+            fprintf(stderr, "error: failed to open '%s' as WAV file\n", fname_inp.c_str());
+            return 5;
+        }
+
+        if (wav.channels != 1 && wav.channels != 2) {
+            fprintf(stderr, "%s: WAV file '%s' must be mono or stereo\n", argv[0], fname_inp.c_str());
+            return 6;
+        }
+
+        if (params.diarize && wav.channels != 2 && params.no_timestamps == false) {
+            fprintf(stderr, "%s: WAV file '%s' must be stereo for diarization and timestamps have to be enabled\n", argv[0], fname_inp.c_str());
+            return 6;
+        }
+
+        if (wav.sampleRate != WHISPER_SAMPLE_RATE) {
+            fprintf(stderr, "%s: WAV file '%s' must be %i kHz\n", argv[0], fname_inp.c_str(), WHISPER_SAMPLE_RATE/1000);
+            return 8;
+        }
+
+        if (wav.bitsPerSample != 16) {
+            fprintf(stderr, "%s: WAV file '%s' must be 16-bit\n", argv[0], fname_inp.c_str());
+            return 9;
+        }
+
+        const uint64_t n = wav_data.empty() ? wav.totalPCMFrameCount : wav_data.size()/(wav.channels*wav.bitsPerSample/8);
+
+        std::vector<int16_t> pcm16;
+        pcm16.resize(n*wav.channels);
+        drwav_read_pcm_frames_s16(&wav, n, pcm16.data());
+        drwav_uninit(&wav);
+
+        // convert to mono, float
+        pcmf32.resize(n);
+        if (wav.channels == 1) {
+            for (uint64_t i = 0; i < n; i++) {
+                pcmf32[i] = float(pcm16[i])/32768.0f;
+            }
+        } else {
+            for (uint64_t i = 0; i < n; i++) {
+                pcmf32[i] = float(pcm16[2*i] + pcm16[2*i + 1])/65536.0f;
             }
         }
 
-        // output stuff
-        {
-            const auto transcription = grandparent / "transcriptions";
-            const auto out_filename = fpath.stem().string() + "-transcription.json";
-            const auto out_path = transcription / out_filename;
-            output_transcription(ctx, out_path.c_str());
+        if (params.diarize) {
+            // convert to stereo, float
+            pcmf32s.resize(2);
+
+            pcmf32s[0].resize(n);
+            pcmf32s[1].resize(n);
+            for (uint64_t i = 0; i < n; i++) {
+                pcmf32s[0][i] = float(pcm16[2*i])/32768.0f;
+                pcmf32s[1][i] = float(pcm16[2*i + 1])/32768.0f;
+            }
         }
     }
+
+    // run the inference
+    {
+        whisper_full_params wparams = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
+
+        wparams.print_realtime   = false;
+        wparams.print_progress   = params.print_progress;
+        wparams.print_timestamps = !params.no_timestamps;
+        wparams.print_special    = params.print_special;
+        wparams.translate        = params.translate;
+        wparams.language         = params.language.c_str();
+        wparams.n_threads        = params.n_threads;
+        wparams.n_max_text_ctx   = params.max_context >= 0 ? params.max_context : wparams.n_max_text_ctx;
+        wparams.offset_ms        = params.offset_t_ms;
+        wparams.duration_ms      = params.duration_ms;
+
+        wparams.token_timestamps = params.output_wts || params.max_len > 0;
+        wparams.thold_pt         = params.word_thold;
+        wparams.max_len          = params.output_wts && params.max_len == 0 ? 60 : params.max_len;
+
+        wparams.speed_up         = params.speed_up;
+
+        wparams.prompt_tokens    = prompt_tokens.empty() ? nullptr : prompt_tokens.data();
+        wparams.prompt_n_tokens  = prompt_tokens.empty() ? 0       : prompt_tokens.size();
+
+        // example for abort mechanism
+        // in this example, we do not abort the processing, but we could if the flag is set to true
+        // the callback is called before every encoder run - if it returns false, the processing is aborted
+        {
+            static bool is_aborted = false; // NOTE: this should be atomic to avoid data race
+
+            wparams.encoder_begin_callback = [](struct whisper_context * /*ctx*/, void * user_data) {
+                bool is_aborted = *(bool*)user_data;
+                return !is_aborted;
+            };
+            wparams.encoder_begin_callback_user_data = &is_aborted;
+        }
+
+        if (whisper_full_parallel(ctx, wparams, pcmf32.data(), pcmf32.size(), params.n_processors) != 0) {
+            fprintf(stderr, "%s: failed to process audio\n", argv[0]);
+            return 10;
+        }
+    }
+
+    output_transcription(ctx, fname_out.c_str());
 
     whisper_free(ctx);
 
