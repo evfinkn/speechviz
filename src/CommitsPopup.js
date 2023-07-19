0d8de4b83413eace65d842abb1ee8b828e978487
evfinkn <evan@finken.name>
2023-07-19 12:13:25 -0500

    Show versions from fossil in CommitsPopup

--- src/CommitsPopup.js
+++ src/CommitsPopup.js
@@ -1,4 +1,37 @@
-import { htmlToElement } from "./util.js";
+import globals from "./globals.js";
+import { htmlToElement, checkResponseStatus } from "./util.js";
+
+const getTimeAgoSpan = (date) => {
+  const now = new Date();
+  const diff = Math.abs(now - date);
+  const minutes = Math.floor(diff / 1000 / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  // undefined so that browser default is used for locale
+  const dateStr = date.toLocaleString(undefined, {
+    dateStyle: "medium",
+    timeStyle: "short",
+  });
+
+  // If the commit is more than 30 days old, show the date
+  if (days > 30) {
+    return `<span class="time-ago">(${dateStr})</span>`;
+  }
+
+  // otherwise, show readable time difference and put the date in a tooltip
+  let timeAgo;
+  if (minutes < 1) {
+    timeAgo = "just now";
+  } else if (minutes < 60) {
+    timeAgo = `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+  } else if (hours < 24) {
+    timeAgo = `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+  } else {
+    timeAgo = `${days} day${days !== 1 ? "s" : ""} ago`;
+  }
+  return `<span class="time-ago" title="${dateStr}">${timeAgo}</span>`;
+};
 
 /** The popup containing extra settings for configuring the interface. */
 const CommitsPopup = class CommitsPopup {
@@ -16,71 +49,6 @@ const CommitsPopup = class CommitsPopup {
   popupContent;
 
   constructor() {
-    // Example data for commits
-    const commits = [
-      {
-        id: 1,
-        message: "Initial commit",
-        author: "John Doe",
-        created: new Date("2023-06-01T10:30:00"),
-        branch: "Head",
-      },
-      {
-        id: 2,
-        message: "Added feature A",
-        author: "Jane Smith",
-        created: new Date("2023-06-02T14:45:00"),
-        branch: "Head",
-      },
-      {
-        id: 3,
-        message: "Fixed bug in feature B",
-        author: "John Doe",
-        created: new Date("2023-06-03T09:15:00"),
-        branch: "Head",
-      },
-      {
-        id: 4,
-        message: "Refactored code",
-        author: "Jane Smith",
-        created: new Date("2023-06-04T16:20:00"),
-        branch: "Head",
-      },
-      {
-        id: 5,
-        message: "Updated documentation",
-        author: "John Doe",
-        created: new Date("2023-06-05T11:10:00"),
-        branch: "Head",
-      },
-      {
-        id: 6,
-        message: "Implemented feature C",
-        author: "Jane Smith",
-        created: new Date("2023-06-06T13:25:00"),
-        branch: "Head",
-      },
-      {
-        id: 7,
-        message: "Initial commit",
-        author: "John Doe",
-        created: new Date("2023-06-01T10:30:00"),
-        branch: "Test",
-      },
-      {
-        id: 8,
-        message: "Initial commit",
-        author: "John Doe",
-        created: new Date("2023-06-01T10:30:00"),
-        branch: "News",
-      },
-    ];
-
-    // Sort commits in reverse chronological order
-    commits.sort(function (a, b) {
-      return b.created - a.created;
-    });
-
     this.popup = htmlToElement("<div class='popup'></div>");
     document.body.append(this.popup);
 
@@ -89,91 +57,85 @@ const CommitsPopup = class CommitsPopup {
     this.popup.appendChild(popupContent);
 
     // Create the branch dropdown
-    var select = document.createElement("select");
-    select.style.float = "left"; // Float the dropdown to the left
+    this.select = document.createElement("select");
+    this.select.style.float = "left"; // Float the dropdown to the left
+    popupContent.appendChild(this.select);
 
-    // In the future populate this with branches from back-end,
-    // then select most recent branch as the default (the 0 index)
-    var options = ["Head", "Test", "News"];
-    for (var i = 0; i < options.length; i++) {
-      var option = document.createElement("option");
-      option.text = options[i];
-      select.add(option);
-    }
-
-    popupContent.appendChild(select);
     const closeButton = htmlToElement("<a class='close'>&times</a>");
     popupContent.appendChild(closeButton);
     popupContent.append(document.createElement("br"));
     popupContent.append(document.createElement("br"));
     closeButton.addEventListener("click", () => this.hide());
 
-    // populate with default of dropdown initially
-    if (select[0])
-      commits
-        .filter(function (commit) {
-          return commit.branch === select[0].value;
-        })
-        .forEach(function (commit) {
-          const commitElement = htmlToElement(`
-              <div class="commit">
-              <div class="message"><a href="https://google.com" class="commit-link">${
-                commit.message
-              }</a></div>
-              <span class="author-name">${
-                commit.author
-              }</span> <span class="time-ago">(${commit.created
-            .toDateString()
-            .split(" ")
-            .slice(1)
-            .join(" ")})</span>
-              </div>
-            `);
+    this.init();
+  }
 
-          // Append commit to commit history
-          popupContent.appendChild(commitElement);
-        });
+  async init() {
+    const versionsUrl = `versions/${globals.basename}-annotations.json`;
+    // Get the commits from the backend
+    const versions = await fetch(versionsUrl)
+      .then(checkResponseStatus)
+      .then((response) => response.json());
+    this.versions = versions;
+
+    // TODO: I don't know if branches needs to be a list
+    const branches = [...new Set(versions.map((ver) => ver.branch))];
+    this.branches = branches;
+    branches.forEach((branch) => {
+      const option = document.createElement("option");
+      option.text = branch;
+      this.select.add(option);
+    });
+
+    if (globals.urlParams.has("commit")) {
+      const commit = globals.urlParams.get("commit");
+      this.currentVersion = versions.find((ver) => ver.commit === commit);
+    } else {
+      // if no commit is specified in the URL, the interface shows the latest commit,
+      // either of any branch (if no branch is specified) or of the specified branch
+      const branch = globals.urlParams.get("branch");
+      if (branch === null) {
+        this.currentVersion = versions[0];
+      } else {
+        this.currentVersion = versions.find((ver) => ver.branch === branch);
+      }
+    }
+    this.currentBranchIndex = branches.indexOf(this.currentVersion.branch);
+
+    const url = new URL(window.location);
+    url.searchParams.delete("branch");
+    versions.forEach((ver) => {
+      // switch the URL to the version's commit
+      url.searchParams.set("commit", ver.commit);
+      ver.datetime = new Date(ver.datetime); // convert from ISO string to Date object
+      ver.element = htmlToElement(`<div class="commit">
+        <div class="message">
+          <a href="${url.toString()}" class="commit-link">${ver.message}</a>
+        </div>
+        <span class="author-name">${ver.user}</span>
+        ${getTimeAgoSpan(ver.datetime)}
+      </div>`);
+      this.popupContent.append(ver.element);
+    });
+
+    this.currentVersion.element.style.border = "3px solid #a4f05d";
 
     // Event listener for dropdown change
-    select.addEventListener("change", function () {
-      // Get the selected branch
-      var selectedBranch = this.value;
+    this.select.addEventListener("change", () => this.updateEntries());
+  }
 
-      // Remove all commit elements from popupContent
-      var commitElements = popupContent.querySelectorAll(".commit");
-      commitElements.forEach(function (element) {
-        element.remove();
-      });
-
-      // Filter and create commit elements for the selected branch
-      commits
-        .filter(function (commit) {
-          return commit.branch === selectedBranch;
-        })
-        .forEach(function (commit) {
-          const commitElement = htmlToElement(`
-            <div class="commit">
-            <div class="message"><a href="https://google.com" class="commit-link">${
-              commit.message
-            }</a></div>
-            <span class="author-name">${
-              commit.author
-            }</span> <span class="time-ago">(${commit.created
-            .toDateString()
-            .split(" ")
-            .slice(1)
-            .join(" ")})</span>
-            </div>
-          `);
-
-          // Append commit to commit history
-          popupContent.appendChild(commitElement);
-        });
+  updateEntries() {
+    const branch = this.select.value;
+    this.versions.forEach((ver) => {
+      ver.element.style.display = ver.branch === branch ? "block" : "none";
     });
   }
 
   /** Updates content and displays this popup. */
   show() {
+    // default the dropdown to the current branch
+    this.select.selectedIndex = this.currentBranchIndex;
+    this.updateEntries();
     this.popup.style.display = "block";
   }
 
