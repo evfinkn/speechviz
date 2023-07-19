721ce1084d224b23768772fdb69b48443ae1a996
blhohmann <blake-hohmann@uiowa.edu>
2023-07-19 04:33:58 -0500

    change to be a function so it can be called in other files

--- scripts/analyze_llm_match_percentages.py
+++ scripts/analyze_llm_match_percentages.py
@@ -1,23 +1,31 @@
 import csv
 
-result_file = "scripts/output/llmMatchPercentages.csv"
 
-total_weighted_percentage = 0
-total_duration = 0
+def output_weighted_avg_percent():
+    result_file = "scripts/output/llmMatchPercentages.csv"
 
-# Read the result file
-with open(result_file, "r") as file:
-    reader = csv.DictReader(file)
+    total_weighted_percentage = 0
+    total_duration = 0
 
-    for row in reader:
-        match_percentage = float(row["Match Percentage"].rstrip("%"))
-        duration = float(row["Duration"].split()[0])
+    # Read the result file
+    with open(result_file, "r") as file:
+        reader = csv.DictReader(file)
 
-        weighted_percentage = match_percentage * duration
-        total_weighted_percentage += weighted_percentage
-        total_duration += duration
+        for row in reader:
+            match_percentage = float(row["Match Percentage"].rstrip("%"))
+            duration = float(row["Duration"].split()[0])
 
-# Calculate the weighted average percentage
-weighted_average_percentage = total_weighted_percentage / total_duration
+            weighted_percentage = match_percentage * duration
+            total_weighted_percentage += weighted_percentage
+            total_duration += duration
 
-print(f"Weighted Average Percentage: {weighted_average_percentage:.2f}%")
+    # Calculate the weighted average percentage
+    weighted_average_percentage = total_weighted_percentage / total_duration
+
+    print(f"Weighted Average Percentage: {weighted_average_percentage:.2f}%")
+
+    return weighted_average_percentage
+
+
+if __name__ == "__main__":
+    output_weighted_avg_percent()
