dd30a00ad2d0f895c59aa5aeec23473dedf6eebb
evfinkn <evan@finken.name>
2023-08-10 13:40:52 -0500

    Simplify viz-route, add support for views

--- routes/viz-route.js
+++ routes/viz-route.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const fs = require("fs");
 const mime = require("mime/lite");
 
@@ -10,82 +10,48 @@ const excludedFiles = new Set([".DS_Store", ".fslckout"]);
 const readdirAndFilter = (path) =>
   fs.readdirSync(path).filter((file) => !excludedFiles.has(file));
 
-const render = (req, res, file, { folder = null, user = null } = {}) => {
-  if (user === null || user === undefined) {
-    user = req.session.user;
-  }
-  res.render("viz", {
-    user: user,
-    file: file,
-    folder: folder,
-    mimetype: mime.getType(file),
-    isVideo: req.query.type === "video",
-    isFolder: folder !== null && folder !== undefined,
-  });
-};
-
-const getUrl = (file, type, { folder = null, user = null } = {}) => {
-  let url = `/viz?file=${file}&type=${type}`;
-  if (folder !== null && folder !== undefined) {
-    url += `&folder=${folder}`;
-  }
-  if (user !== null && user !== undefined) {
-    url += `&user=${user}`;
-  }
-  return url;
-};
-
 /* GET home page. */
 router.get("/", (req, res) => {
-  const file = req.query.file;
-  const folder = req.query.folder;
-  const user = req.query.user;
+  const { file, folder, type } = req.query;
   // for displaying clustered faces
-  if (req.query.file !== undefined) {
+  if (file) {
     // sometimes when accessing a page
     // through a referrer file isn't set
     req.session.dir = req.query.file.replace(/\.[^/.]+$/, "");
     req.session.inFaceFolder = false;
   }
 
-  if (req.query.type === "video") {
-    if (folder !== undefined && folder !== null) {
-      if (fs.readdirSync("data/video/" + folder)) {
-        if (file === undefined || file === null) {
-          // the first time you click a folder there will be no file
-          const firstFile = readdirAndFilter("data/video/" + folder).at(0);
-          res.redirect(getUrl(firstFile, "video", { folder, user }));
-        }
-        render(req, res, file, { folder });
-      } else {
-        res.redirect("/");
-      }
-    } else {
-      if (fs.readdirSync("data/video").includes(file)) {
-        render(req, res, file);
-      } else {
-        res.redirect("/");
-      }
+  if (!type || (!file && !folder)) {
+    // invalid query string so can't get anything to show
+    res.redirect("/");
+  } else if (folder && !file) {
+    // viewing a folder but no file was given, so
+    // redirect to the first file in the folder
+    const folderPath = `data/${type}/${folder}`;
+    try {
+      const files = readdirAndFilter(folderPath);
+      const url =
+        files.length === 0
+          ? "/" // folder is empty so nothing to show
+          : `/viz?file=${files[0]}&type=${type}&folder=${folder}`;
+      res.redirect(url);
+    } catch {
+      res.redirect("/"); // folder doesn't exist
     }
   } else {
-    if (folder !== undefined && folder !== null) {
-      // we have opened a folder
-      if (fs.readdirSync("data/audio/" + folder)) {
-        if (file === undefined || file === null) {
-          // the first time you click a folder there will be no file
-          const firstFile = readdirAndFilter("data/audio/" + folder).at(0);
-          res.redirect(getUrl(firstFile, "audio", { folder, user }));
-        }
-        render(req, res, file, { folder });
-      } else {
-        res.redirect("/");
-      }
+    const path = folder ? `${type}/${folder}/${file}` : `${type}/${file}`;
+    // can't use try catch like above because we don't need to do any IO on the file
+    // so we can just check if it exists
+    if (!fs.existsSync(`data/${path}`)) {
+      res.redirect("/");
     } else {
-      if (fs.readdirSync("data/audio").includes(file)) {
-        render(req, res, file, { folder });
-      } else {
-        res.redirect("/");
-      }
+      res.render("viz", {
+        user: req.session.user,
+        type,
+        file,
+        path,
+        mimetype: mime.getType(file),
+      });
     }
   }
 });
