0e7f617cc3068a60874b60dff2f8f704e6d47de6
evfinkn <evan@finken.name>
2023-10-19 08:59:02 -0500

    refactor: simplify index by using pug mixins

--- routes/index-route.js
+++ routes/index-route.js
@@ -1,8 +1,26 @@
+import path from "path";
+
 import express from "express";
+
+import { dataDir } from "../server/globals.js";
+import { readdirAndFilter } from "../server/io.js";
+
+const audioDir = path.join(dataDir, "audio");
+const videoDir = path.join(dataDir, "video");
+const viewsDir = path.join(dataDir, "views");
+const faceClusterDir = path.join(dataDir, "faceClusters");
 
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res) => res.render("index"));
+router.get("/", async (req, res) => {
+  const audios = await readdirAndFilter(audioDir);
+  const videos = await readdirAndFilter(videoDir);
+  const views = (await readdirAndFilter(viewsDir)).filter(
+    (viewFile) => !viewFile.endsWith("-times.csv"),
+  );
+  const faces = await readdirAndFilter(faceClusterDir);
+  res.render("index", { audios, videos, views, faces });
+});
 
 export default router;
